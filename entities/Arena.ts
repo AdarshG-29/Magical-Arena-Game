16f8469eba4169bc69f09a625315ea241b42edef
AdarshG-29 <adarsh2625@gmail.com>
2024-06-30 01:12:50 +0530

    added play method in arena entity

--- entities/Arena.ts
+++ entities/Arena.ts
@@ -1,6 +1,6 @@
 //defining arena class entity and all the required methods.
 
-import { inValidAttribute } from "../utils/helper";
+import { inValidAttribute, rollDice } from "../utils/helper";
 import { Player } from "./Player";
 
 export class Arena {
@@ -66,5 +66,54 @@ export class Arena {
       console.log(`Strength: ${strength}`);
       console.log(`Attack: ${attack}`);
     }
+  }
+
+  play(id_first_player: number, id_second_player: number) {
+    if (id_first_player === id_second_player) {
+      console.log("Ids can not be same for both the players.\n");
+      return;
+    }
+    let attacker = this.Players.get(id_first_player);
+    let defender = this.Players.get(id_second_player);
+    if (!attacker) {
+      console.log(`Player with id: ${id_second_player} does not exist.\n`);
+      return;
+    }
+    if (!defender) {
+      console.log(`Player with id: ${id_first_player} does not exist.\n`);
+      return;
+    }
+    //swapping attacker and defender with respect to their health. As lower health player will attack first.
+    if (defender?.health < attacker?.health) {
+      [attacker, defender] = [defender, attacker];
+    }
+
+    while (defender.health > 0) {
+      const attackerRolledDice = rollDice();
+      const attacking_power = attacker.attack * attackerRolledDice;
+      const defenderRolledDice = rollDice();
+      const defending_power = defender.strength * defenderRolledDice;
+
+      console.log(
+        `${attacker.name} rolled dice of ${attackerRolledDice} and attacks with the power ${attacking_power}`
+      );
+      console.log(
+        `${defender.name} rolled dice of ${defenderRolledDice} and defends with the strength ${defending_power}`
+      );
+
+      console.log("After Attack:\n");
+
+      if (attacking_power > defending_power)
+        defender.health = Math.max(
+          0,
+          defender.health - (attacking_power - defending_power)
+        ); //if attacking power is more than defender's strength, then we will deduct it from defender's health.
+
+      console.log(`${defender.name} has health: ${defender.health}\n`);
+
+      if (defender.health > 0) [attacker, defender] = [defender, attacker]; //the if condition implies that finally, the current attacker will win and the current defender will loose, and it wont swap it again after loosing the game.
+    }
+    console.log(`${attacker.name} is winner\n`);
+    this.eliminatePlayer(defender.playerId);
   }
 }
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,3 +1,8 @@
 export function inValidAttribute(attribute: number): boolean {
   return !Number.isInteger(attribute) || attribute <= 0;
 }
+export function rollDice(): number {
+  const minValue = 1,
+    maxValue = 6;
+  return minValue + Math.floor(Math.random() * (maxValue - minValue + 1));
+}
